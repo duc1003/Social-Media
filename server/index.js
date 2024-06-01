b552ef851508cf9fe61f6a2e47f53bc49e2b620d
ducc0506 <ducnd.ntd.vn@gmail.com>
2024-06-01 15:37:32 +0700

    attach register and login

--- server/index.js
+++ server/index.js
@@ -1,15 +1,26 @@
 import express from 'express';
 import cors from 'cors';
-const app = express();
 
 import {usersRoutes , authRoutes, commentsRoutes, postsRoutes, relationshipsRoutes, storiesRoutes} from "./routes/index.js";
 import cookieParser from 'cookie-parser';
+import "dotenv/config.js"
 
 
+const app = express();
 
 // MIDDLEWARE
+app.use((req, res, next)=> {
+    res.header("Access-Control-Allow-Origin", true);
+    next();
+})
 app.use(express.json());
-app.use(cors());
+
+const corsOptions = {
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 app.use(cookieParser());
 
 
@@ -20,6 +31,6 @@ app.use("/api/post", postsRoutes);
 app.use("/api/relationship", relationshipsRoutes);
 app.use("/api/story", storiesRoutes);
 
-app.listen(8080, () => {
+app.listen(process.env.PORT_SERVER, () => {
     console.log(`Server started on 8080`);
 });
