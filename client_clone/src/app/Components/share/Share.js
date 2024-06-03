0d1c6fa5f87448fb2564b79694f02a986d0db32a
hkt04102003 <hoangkimthinh12b@gmail.com>
2024-06-03 15:57:01 +0700

    "COMPLETE STORIES AND SHARE"

--- client_clone/src/app/Components/share/Share.js
+++ client_clone/src/app/Components/share/Share.js
@@ -1,9 +1,47 @@
 import React from 'react'
 import styles from './share.module.css'
+import Images from "../../assets/img.png";
+import Map from "../../assets/map.png";
+import Friend from "../../assets/friend.png";
+import Image from 'next/image'
 const Share = () => {
-  return (
-    <div>Share</div>
-  )
-}
 
-export default Share
+  return (
+    <div className={styles.share}>
+      <div className={styles.container}>
+        <div className={styles.top}>
+          <img
+            src="https://png.pngtree.com/thumb_back/fh260/background/20210416/pngtree-purple-starry-gentle-gentle-and-colorful-clouds-background-image_619990.jpg"
+            alt=""
+          />
+          <input type="text" placeholder={`What's on your mind Thịnh?`} />
+        </div>
+        <hr />
+        <div className={styles.bottom}>
+          <div className={styles.left}>
+            <input type="file" id="file" style={{display:"none"}} />
+            <label htmlFor="file">
+              <div className={styles.item}>
+                <Image src={Images} alt="" width = {20} height = {20}/>
+                <span>Add Image</span>
+              </div>
+            </label>
+            <div className={styles.item}>
+              <Image src={Map} alt="" width = {20} height = {20}/>
+              <span>Add Place</span>
+            </div>
+            <div className={styles.item}>
+              <Image src={Friend} alt="" width = {20} height = {20} />
+              <span>Tag Friends</span>
+            </div>
+          </div>
+          <div className={styles.right}>
+            <button>Share</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Share;
--- client_clone/src/app/Components/stories/Stories.js
+++ client_clone/src/app/Components/stories/Stories.js
@@ -1,8 +1,49 @@
-import React from 'react'
-import styles from './stories.module.css'
+
+
+import styles from './stories.module.css';
+
 const Stories = () => {
+
+
+
+  //TEMPORARY
+  const stories = [
+    {
+      id: 1,
+      name: "John Doe",
+      img: "https://png.pngtree.com/thumb_back/fh260/background/20210416/pngtree-purple-starry-gentle-gentle-and-colorful-clouds-background-image_619990.jpg",
+    },
+    {
+      id: 2,
+      name: "John Doe",
+      img: "https://png.pngtree.com/thumb_back/fh260/background/20210416/pngtree-purple-starry-gentle-gentle-and-colorful-clouds-background-image_619990.jpg",
+    },
+    {
+      id: 3,
+      name: "John Doe",
+      img: "https://png.pngtree.com/thumb_back/fh260/background/20210416/pngtree-purple-starry-gentle-gentle-and-colorful-clouds-background-image_619990.jpg",
+    },
+    {
+      id: 4,
+      name: "John Doe",
+      img: "https://png.pngtree.com/thumb_back/fh260/background/20210416/pngtree-purple-starry-gentle-gentle-and-colorful-clouds-background-image_619990.jpg",
+    },
+  ];
+
   return (
-    <div>Stories</div>
+    <div className={styles.stories}>
+      <div className={styles.story}>
+          <img src="https://png.pngtree.com/thumb_back/fh260/background/20210416/pngtree-purple-starry-gentle-gentle-and-colorful-clouds-background-image_619990.jpg" alt="" />
+          <span>Thịnh</span>
+          <button>+</button>
+        </div>
+      {stories.map(story=>(
+        <div className={styles.story} key={story.id}>
+          <img src={story.img} alt="" />
+          <span>{story.name}</span>
+        </div>
+      ))}
+    </div>
   )
 }
 
