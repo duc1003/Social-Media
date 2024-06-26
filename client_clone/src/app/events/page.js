629eb56a19b5352da1915c6eaf05e9613d35b03b
hkt04102003 <hoangkimthinh12b@gmail.com>
2024-06-26 15:46:06 +0700

    "FIX EVENT #1"

--- client_clone/src/app/events/page.js
+++ client_clone/src/app/events/page.js
@@ -1,23 +1,34 @@
 "use client";
 import { useState, useEffect, useRef } from "react";
 import styles from "./events.module.css";
-import BoxEvent from '../../Components/events/boxEvent/BoxEvent'
+import BoxEvent from "../../Components/events/boxEvent/BoxEvent";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
 import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
-import SearchIcon from '@mui/icons-material/Search';
-import NearMeIcon from '@mui/icons-material/NearMe';
-import LanguageIcon from '@mui/icons-material/Language';
+import SearchIcon from "@mui/icons-material/Search";
+import NearMeIcon from "@mui/icons-material/NearMe";
+import LanguageIcon from "@mui/icons-material/Language";
 import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
 import dayjs from "dayjs";
 
 export default function Events() {
   const [value, setValue] = useState(dayjs("2022-04-17"));
   const [closeCalendar, setCloseCalendar] = useState(false);
+  //
   const [openPopper, setOpenPopper] = useState(false);
-  
+  const [selectedLocation, setSelectedLocation] = useState("Vị trí của tôi");
+  //search
+  const [activeInput, setActiveInput] = useState(0);
+  const [cityName, setCityName] = useState("");
+  const [suggestions, setSuggestions] = useState([]);
+  const [filteredSuggestions, setFilteredSuggestions] = useState([]);
+  const [openFocus, setOpenFocus] = useState(false);
+  //calendar
   const calendarRef = useRef(null);
   const popperRef = useRef(null);
-
+  const apiKey = "YwpoqLrxaML5NkwdB7tHiA==sw9XBw5h4HllMXLH";
+  const handleLocation = (e) => {
+    setSelectedLocation(e);
+  };
   const handleOpenCalendar = () => {
     setCloseCalendar(!closeCalendar);
   };
@@ -25,6 +36,59 @@ export default function Events() {
   const handleOpenPopper = () => {
     setOpenPopper(!openPopper);
   };
+  const handleFocus = () => {
+    setOpenFocus(true);
+  };
+
+  //api city
+  useEffect(() => {
+    if (cityName.length > 0) {
+      fetch(`https://api.api-ninjas.com/v1/city?name=${cityName}&country=vn`, {
+        method: "GET",
+        headers: { "X-Api-Key": apiKey },
+        contentType: "application/json",
+      })
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error(
+              "Network response was not ok " + response.statusText
+            );
+          }
+          return response.json();
+        })
+        .then((result) => {
+          setSuggestions(result);
+        })
+        .catch((error) => {
+          setSuggestions([]);
+        });
+    } else {
+      setSuggestions([]);
+    }
+  }, [cityName, apiKey]);
+
+  useEffect(() => {
+    if (cityName.length > 0) {
+      const filtered = suggestions.filter((city) =>
+        city.name.toLowerCase().startsWith(cityName.toLowerCase())
+      );
+      setFilteredSuggestions(filtered);
+    } else {
+      setFilteredSuggestions([]);
+    }
+  }, [cityName, suggestions]);
+  const handleInputChange = (e) => {
+    setCityName(e.target.value);
+  };
+
+  const handleSuggestionClick = (city) => {
+    console.log(city);
+    setCityName("");
+    setFilteredSuggestions([]);
+    setOpenPopper(false);
+    setSelectedLocation(city.name);
+  };
+  //
 
   useEffect(() => {
     const handleClickOutside = (event) => {
@@ -33,6 +97,7 @@ export default function Events() {
       }
       if (popperRef.current && !popperRef.current.contains(event.target)) {
         setOpenPopper(false);
+        setOpenFocus(false);
       }
     };
 
@@ -42,7 +107,7 @@ export default function Events() {
       document.removeEventListener("mousedown", handleClickOutside);
     };
   }, []);
-
+  console.log(value);
   return (
     <div className={styles.home}>
       <div className={styles.homeContent}>
@@ -50,33 +115,80 @@ export default function Events() {
         <div className={styles.boxFilter}>
           <div className={styles.filter} onClick={handleOpenPopper}>
             <LocationOnIcon className={styles.filterIcon} />
-            <p>Vị trí của tôi</p>
+            <p>{selectedLocation}</p>
           </div>
           {openPopper ? (
             <div ref={popperRef} className={styles.filterLocation}>
               <div className={styles.search}>
-                <input type="text" className={styles.input} placeholder = "Tìm kiếm theo thành phố" checked = {true}/>
-                <SearchIcon className={styles.searchIcon}/>
+                <input
+                  type="text"
+                  className={styles.input}
+                  placeholder="Tìm kiếm theo thành phố"
+                  onFocus={handleFocus}
+                  value={cityName}
+                  onChange={handleInputChange}
+                />
+                <SearchIcon className={styles.searchIcon} />
+                {filteredSuggestions.length > 0 ? (
+                  <div className={styles.popperSearch}>
+                    {filteredSuggestions.map((city, index) => (
+                      <div
+                        key={index}
+                        className={styles.searchLocation}
+                        onClick={() => handleSuggestionClick(city)}
+                      >
+                        <LocationOnIcon />
+                        <p>{city.name}</p>
+                      </div>
+                    ))}
+                  </div>
+                ) : (
+                  <></>
+                )}
               </div>
               <div className={styles.filterRadio}>
-                <label htmlFor="filterRadio1" className={styles.boxInputLocation}>
-                  
+                <label
+                  htmlFor="filterRadio1"
+                  className={styles.boxInputLocation}
+                >
                   <label htmlFor="filterRadio1">
-                    <NearMeIcon/>
+                    <NearMeIcon />
                     <p>Vị trí của tôi</p>
                   </label>
-                  <input type="radio" id="filterRadio1" style={{ width: "18px" }} name="filterRadio1" />
+                  <input
+                    type="radio"
+                    id="filterRadio1"
+                    style={{ width: "18px" }}
+                    name="filterRadio"
+                    checked={activeInput === 0}
+                    onChange={(e) => {
+                      setActiveInput(0);
+                      handleLocation("Vị trí của tôi");
+                    }}
+                  />
                 </label>
-                <hr/>
-                <label htmlFor="filterRadio2" className={styles.boxInputLocation} style={{ margin: "0" }}>
-                  
+                <hr />
+                <label
+                  htmlFor="filterRadio2"
+                  className={styles.boxInputLocation}
+                  style={{ margin: "0" }}
+                >
                   <label htmlFor="filterRadio2">
-                    <LanguageIcon/>
+                    <LanguageIcon />
                     <p>Online</p>
                   </label>
-                  <input type="radio" id="filterRadio2" style={{ width: "18px" }} name="filterRadio1" />
+                  <input
+                    type="radio"
+                    id="filterRadio2"
+                    style={{ width: "18px" }}
+                    name="filterRadio"
+                    checked={activeInput === 1}
+                    onChange={(e) => {
+                      setActiveInput(1);
+                      handleLocation("Online");
+                    }}
+                  />
                 </label>
-                
               </div>
             </div>
           ) : (
@@ -108,13 +220,13 @@ export default function Events() {
           )}
         </div>
         <div className={styles.boxEvent}>
-          <BoxEvent id = {"1"}/>
-          <BoxEvent id = {"1"}/>
-          <BoxEvent id = {"1"}/>
-          <BoxEvent id = {"1"}/>
-          <BoxEvent id = {"1"}/>
-          <BoxEvent id = {"1"}/>
-          <BoxEvent id = {"1"}/>
+          <BoxEvent id={"1"} />
+          <BoxEvent id={"1"} />
+          <BoxEvent id={"1"} />
+          <BoxEvent id={"1"} />
+          <BoxEvent id={"1"} />
+          <BoxEvent id={"1"} />
+          <BoxEvent id={"1"} />
         </div>
       </div>
     </div>
--- client_clone/src/app/memories/hidePerson/page.js
+++ client_clone/src/app/memories/hidePerson/page.js
@@ -1,13 +1,18 @@
-import React from 'react'
+'use client'
+import React,{useState} from 'react'
 import styles from './hidePerson.module.css'
 import { Input } from '@mui/material'
 const page = () => {
+  const [name,setName] = useState('')
+  const  handleName =(e)=>{
+    setName(e.target.value)
+  }
   return (
     <div className={styles.container}>
       <h4 className={styles.title}>Ẩn Kỷ niệm về mọi người</h4>
       <p className={styles.content}>Hãy cho biết bạn không muốn thấy ai trong kỷ niệm của mình. Chúng tôi sẽ không gửi thông báo cho họ.</p>
-      <input  placeholder='Bắt đầu nhập tên'/>
-      <button>Lưu</button>
+      <input  placeholder='Bắt đầu nhập tên' value={name} onChange={handleName}/>
+      <button className={`${name !== "" ? styles.active : ''}`}>Lưu</button>
     </div>
   )
 }
